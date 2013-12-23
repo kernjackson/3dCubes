c2820211300bc0722e15336cfd5191cde76304b1
Kern Jackson <kern@PizzaParty.local>
2013-12-23 15:18:19 -0600

    Initial Commit

---
+++ SimpleGLKitGame/SGGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGGAppDelegate.h
+//  SimpleGLKitGame
+//
+//  Created by Kern Jackson on 12/23/13.
+//  Copyright (c) 2013 Kern Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleGLKitGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleGLKitGame
+//
+//  Created by Kern Jackson on 12/23/13.
+//  Copyright (c) 2013 Kern Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGGAppDelegate class]));
+    }
+}
